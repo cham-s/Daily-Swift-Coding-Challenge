a9cc0174f86ffa8ce9ff8f821b40906fddd570a7
cham-s <cc.attou@gmail.com>
2018-11-24 16:01:21 +0100

    1st version of c4

--- Challenge4/Sources/Challenge4/Challenge4.swift
+++ Challenge4/Sources/Challenge4/Challenge4.swift
@@ -1,3 +1,9 @@
-struct Challenge4 {
-    var text = "Hello, World!"
+import Foundation
+
+
+extension String {
+    func challenge4(_ compare: String) -> Bool {
+        let lowercased = self.lowercased()
+        return lowercased.range(of: compare) != nil
+    }
 }
--- Challenge4/Tests/Challenge4Tests/Challenge4Tests.swift
+++ Challenge4/Tests/Challenge4Tests/Challenge4Tests.swift
@@ -2,14 +2,26 @@ import XCTest
 @testable import Challenge4
 
 final class Challenge4Tests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Challenge4().text, "Hello, World!")
+    func firstTest() {
+        XCTAssertTrue("Hello, world".challenge4("Hello"))
+    }
+    
+    func secondTest() {
+        XCTAssertTrue("J'ai achete du lait".challenge4("Lait"))
+    }
+    
+    func thirdTest() {
+        XCTAssertTrue("今日は暑いですね".challenge4("暑い"))
+    }
+    
+    func fourthTest() {
+        XCTAssertFalse("Hello, Waldo".challenge4("World"))
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("first Test", firstTest),
+        ("second Test", firstTest),
+        ("third Test", firstTest),
+        ("fourth Test", firstTest),
     ]
 }
