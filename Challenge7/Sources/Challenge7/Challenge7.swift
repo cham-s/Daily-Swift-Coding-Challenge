d16e8c76fb4faae1daea653adfc3f82f7b799c61
cham-s <cc.attou@gmail.com>
2018-11-24 23:22:55 +0100

    minor update

--- Challenge7/Sources/Challenge7/Challenge7.swift
+++ Challenge7/Sources/Challenge7/Challenge7.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public func challenge7(input: String) -> String {
-    let seperated = input.components(separatedBy: " ")
+    let seperated = input.components(separatedBy: .whitespaces)
     let filtered = seperated.filter { !$0.isEmpty }
     return filtered.joined(separator: " ")
 }
