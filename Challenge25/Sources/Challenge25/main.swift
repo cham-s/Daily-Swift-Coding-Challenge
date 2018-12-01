63c56285439c44b18202bf31e52e43f2a4233e32
cham-s <cc.attou@gmail.com>
2018-12-01 07:53:07 +0100

    2nd ver c25

--- Challenge25/Sources/Challenge25/main.swift
+++ Challenge25/Sources/Challenge25/main.swift
@@ -1,24 +1,14 @@
 import Foundation
 
 func challenge28(filename: String, message: String) throws {
-    let fm = FileManager.default
-    if fm.fileExists(atPath: filename) {
-        let content = try? String(contentsOfFile: filename)
-        if var content = content {
-            content += "\(message) \(Date())\n"
-            do {
-               try content.write(toFile: filename, atomically: true, encoding: .utf8)
-            }
-        }
-    } else {
+    do {
+        var content = (try? String(contentsOfFile: filename)) ?? ""
+        content += "\(message) \(Date())\n"
         do {
-            let log = "\(message) \(Date())\n"
-            fm.createFile(atPath: filename, contents: nil, attributes: nil)
-            try log.write(toFile: filename, atomically: true, encoding: .utf8)
+            try content.write(toFile: filename, atomically: true, encoding: .utf8)
         }
     }
 }
-
 
 do {
     try challenge28(filename: "./log", message: "error 408")
