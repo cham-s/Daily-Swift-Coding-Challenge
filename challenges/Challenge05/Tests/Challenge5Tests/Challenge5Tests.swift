5e74e2a3c05733446630be77bf055af803822cc3
cham-s <cc.attou@gmail.com>
2018-12-05 18:54:10 +0100

    updated test for c5

--- challenges/Challenge05/Tests/Challenge5Tests/Challenge5Tests.swift
+++ challenges/Challenge05/Tests/Challenge5Tests/Challenge5Tests.swift
@@ -2,16 +2,16 @@ import XCTest
 @testable import Challenge5
 
 final class Challenge5Tests: XCTestCase {
-    func firstTest() {
+    func testFirst() {
         XCTAssertEqual(challenge5(input: "Mon train est arrive en retard.", count: "a"), 3)
     }
     
-    func secondTest() {
+    func testSecond() {
         XCTAssertEqual(challenge5(input: "Today is the day.", count: "y"), 2)
     }
     
     static var allTests = [
-        ("first test", firstTest),
-        ("second test", secondTest)
+        ("first test", testFirst),
+        ("second test", testSecond)
     ]
 }
