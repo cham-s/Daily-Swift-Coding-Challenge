e4eab8f69e863e326e264f97b5f7bdb4a97ced48
cham-s <cc.attou@gmail.com>
2018-12-05 18:47:50 +0100

    updated test [ci skip]

--- challenges/Challenge04/Tests/Challenge4Tests/Challenge4Tests.swift
+++ challenges/Challenge04/Tests/Challenge4Tests/Challenge4Tests.swift
@@ -2,26 +2,26 @@ import XCTest
 @testable import Challenge4
 
 final class Challenge4Tests: XCTestCase {
-    func firstTest() {
+    func testFirst() {
         XCTAssertTrue("Hello, world".challenge4("Hello"))
     }
     
-    func secondTest() {
+    func testSecond() {
         XCTAssertTrue("J'ai achete du lait".challenge4("Lait"))
     }
     
-    func thirdTest() {
+    func testThird() {
         XCTAssertTrue("今日は暑いですね".challenge4("暑い"))
     }
     
-    func fourthTest() {
+    func testFourth() {
         XCTAssertFalse("Hello, Waldo".challenge4("World"))
     }
 
     static var allTests = [
-        ("first Test", firstTest),
-        ("second Test", firstTest),
-        ("third Test", firstTest),
-        ("fourth Test", firstTest),
+        ("first Test", testFirst),
+        ("second Test", testSecond),
+        ("third Test", testThird),
+        ("fourth Test", testFourth)
     ]
 }
