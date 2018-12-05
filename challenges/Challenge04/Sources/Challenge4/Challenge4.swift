e0dedf711a116e7109c6d9c120bdb6cf4710d65c
cham-s <cc.attou@gmail.com>
2018-12-05 18:51:23 +0100

    updated code of c4

--- challenges/Challenge04/Sources/Challenge4/Challenge4.swift
+++ challenges/Challenge04/Sources/Challenge4/Challenge4.swift
@@ -4,6 +4,6 @@ import Foundation
 extension String {
     func challenge4(_ compare: String) -> Bool {
         let lowercased = self.lowercased()
-        return lowercased.range(of: compare) != nil
+        return lowercased.range(of: compare.lowercased()) != nil
     }
 }
