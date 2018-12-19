a3ec28ac01febdc462417c9308e8fc831f9fba28
cham-s <cc.attou@gmail.com>
2018-12-19 18:03:43 +0100

    fixed try problem

--- challenges/Challenge37/Sources/Challenge37/Challenge37.swift
+++ challenges/Challenge37/Sources/Challenge37/Challenge37.swift
@@ -1,7 +1,10 @@
 extension Collection {
     func challenge37<T>(transform: (Element) throws -> T) rethrows -> [T] {
         var result = [T]()
-        forEach { result.append(transform($0)) }
+
+        for element in self {
+            result.append(try transform(element))
+        }
         return result
     }
 }
