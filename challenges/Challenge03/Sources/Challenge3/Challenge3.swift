4d9f167f9a5765c08dd69cb04e2dae85ca26d012
cham-s <cc.attou@gmail.com>
2018-12-24 19:44:50 +0100

    redo c3

--- challenges/Challenge03/Sources/Challenge3/Challenge3.swift
+++ challenges/Challenge03/Sources/Challenge3/Challenge3.swift
@@ -1,9 +1,7 @@
+import Foundation
 
-
-public func challenge3(first: String, second: String) -> Bool {
+func challenge3(first: String, second: String) -> Bool {
     guard first.count == second.count else { return false }
-    
-    let setFirst = Set(first)
-    let setSecond = Set(second)
-    return setFirst.symmetricDifference(setSecond).count == 0
+    let firstSet = Set(Array(first))
+    return firstSet.symmetricDifference(Set(second)).count == 0
 }
