bcad55ae626b26b83d25f889e8077e884fb93482
cham-s <cc.attou@gmail.com>
2018-12-24 19:15:45 +0100

    redo c1

--- challenges/Challenge01/Sources/unique/unique.swift
+++ challenges/Challenge01/Sources/unique/unique.swift
@@ -1,21 +1,7 @@
-
-//
-//  unique.swift
-//  unique
-//
-//  Created by chams on 22/11/2018.
-//
-
 import Foundation
 
-
-
-public func challenge1(input: String) -> Bool {
-    
-    let inputArray = Array(input)
-    let inputSet = NSCountedSet(array: inputArray)
-    if let _ = inputArray.firstIndex(where: { inputSet.count(for: $0) > 1 }) {
-        return false
-    }
-    return true
+func challenge1(input: String) -> Bool {
+    let stringSet = Set(Array(input))
+    return stringSet.count == input.count
 }
+
