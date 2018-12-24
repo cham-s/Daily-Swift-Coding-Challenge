29882431f17b14bbb24286dba0cc3a785f313660
cham-s <cc.attou@gmail.com>
2018-12-24 19:35:44 +0100

    redo c2

--- challenges/Challenge02/Sources/Challenge2/Challenge2.swift
+++ challenges/Challenge02/Sources/Challenge2/Challenge2.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-public func challenge2(input: String) -> Bool {
-    let lowercased = input.lowercased()
-    return lowercased == String(lowercased.reversed())
+func challenge2(input: String) -> Bool {
+    return String(input.reversed()).lowercased() == input.lowercased()
 }
