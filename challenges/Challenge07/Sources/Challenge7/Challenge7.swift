31a2f43ae1dccf3ae84e3debf0e3e1bd8a2cc8de
cham-s <cc.attou@gmail.com>
2018-12-26 20:52:55 +0100

    redo c7

--- challenges/Challenge07/Sources/Challenge7/Challenge7.swift
+++ challenges/Challenge07/Sources/Challenge7/Challenge7.swift
@@ -1,7 +1,10 @@
 import Foundation
 
-public func challenge7(input: String) -> String {
-    let seperated = input.components(separatedBy: .whitespaces)
-    let filtered = seperated.filter { !$0.isEmpty }
-    return filtered.joined(separator: " ")
+func challenge7(input: String) -> String {
+    guard !input.isEmpty else {
+        return ""
+    }
+    
+    let components = input.components(separatedBy: .whitespaces)
+    return components.filter { !$0.isEmpty }.joined(separator: " ")
 }
