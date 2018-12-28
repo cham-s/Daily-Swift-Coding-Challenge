e6be0dca1b718bfb903157372035dea3d86e77e8
cham-s <cc.attou@gmail.com>
2018-12-28 22:19:45 +0100

    redo c13

--- challenges/Challenge13/Sources/Challenge13/Challenge13.swift
+++ challenges/Challenge13/Sources/Challenge13/Challenge13.swift
@@ -2,7 +2,9 @@ import Foundation
 
 func challenge13(input: String) -> String {
     guard !input.isEmpty else { return "" }
+    
     return input.components(separatedBy: " ")
+        .filter { !$0.isEmpty }
         .map { String($0.reversed()) }
         .joined(separator: " ")
 }
