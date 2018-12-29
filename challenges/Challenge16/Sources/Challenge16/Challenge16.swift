f16ad09be015b646665c9c30a9e1cd8f11e75b49
cham-s <cc.attou@gmail.com>
2018-12-29 22:42:54 +0100

    c16

--- challenges/Challenge16/Sources/Challenge16/Challenge16.swift
+++ challenges/Challenge16/Sources/Challenge16/Challenge16.swift
@@ -1,5 +1,6 @@
+import Foundation
+
 func challenge16(_ number: Int, power: Int) -> Int {
-    guard number > 0, power > 0 else { return 0 }
-    if power == 1 { return number }
-    return challenge16(number, power: power - 1) * number
+    guard power > 1 else { return number }
+    return number * challenge16(number , power: power - 1)
 }
