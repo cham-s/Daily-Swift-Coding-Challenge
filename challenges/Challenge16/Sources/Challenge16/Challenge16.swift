b8efbc7ed7b3793e39646f4d24c51bd5a3bc9558
cham-s <cc.attou@gmail.com>
2018-12-29 22:46:09 +0100

    forgot case for 1

--- challenges/Challenge16/Sources/Challenge16/Challenge16.swift
+++ challenges/Challenge16/Sources/Challenge16/Challenge16.swift
@@ -3,5 +3,6 @@ import Foundation
 func challenge16(_ number: Int, power: Int) -> Int {
     guard power > 0,
           number > 0 else { return 0 }
+    if power == 1 { return number }
     return number * challenge16(number , power: power - 1)
 }
