11badb90296fe9807edaad15a71e31a3d7b743b1
cham-s <cc.attou@gmail.com>
2019-01-04 05:47:13 +0100

    added more solutions for c23

--- challenges/Challenge23/Sources/Challenge23/Challenge23.swift
+++ challenges/Challenge23/Sources/Challenge23/Challenge23.swift
@@ -1,3 +1,15 @@
 func challenge23(subtract: Int, from: Int) -> Int {
     return (subtract..<from).count
 }
+
+func challenge23b(subtract: Int, from: Int) -> Int {
+    return from + -subtract
+}
+
+func challenge23c(subtract: Int, from: Int) -> Int {
+    return from + -1 * subtract
+}
+
+func challenge23d(subract: Int, from: Int) -> Int {
+    return from + (~subract + 1)
+}
