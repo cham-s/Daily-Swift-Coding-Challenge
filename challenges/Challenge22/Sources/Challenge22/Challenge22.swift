c37aa3a53f29468050538e65ca09ac03b77e8c30
cham-s <cc.attou@gmail.com>
2019-01-04 05:00:21 +0100

    remake c 22

--- challenges/Challenge22/Sources/Challenge22/Challenge22.swift
+++ challenges/Challenge22/Sources/Challenge22/Challenge22.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 func challenge22(_ input: String) -> Int {
-    let components = input.components(separatedBy: CharacterSet.decimalDigits.inverted)
-    return components.compactMap{ Int($0) }.reduce(0, +)
+    return input.components(separatedBy: CharacterSet.decimalDigits.inverted)
+           .compactMap { Int($0) }
+           .reduce(0, +)
 }
