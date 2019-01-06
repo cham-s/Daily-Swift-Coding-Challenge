d3e891c87cef0c72b8910c1874cc23fc3d8696ef
cham-s <cc.attou@gmail.com>
2019-01-06 22:18:03 +0100

    remake c24

--- challenges/Challenge25/Sources/Challenge25/main.swift
+++ challenges/Challenge25/Sources/Challenge25/main.swift
@@ -1,18 +1,12 @@
 import Foundation
 
-func challenge28(filename: String, message: String) throws {
-    do {
-        var content = (try? String(contentsOfFile: filename)) ?? ""
-        content += "\(message) \(Date())\n"
-        do {
-            try content.write(toFile: filename, atomically: true, encoding: .utf8)
-        }
-    }
+func log(message: String, to filename: String) {
+    var logFile = (try? String(contentsOfFile: filename)) ?? ""
+    logFile.append("\(Date()): \(message)")
+    _ = try? logFile.write(toFile: filename,
+                           atomically: true,
+                           encoding: .utf8)
 }
 
-do {
-    try challenge28(filename: "./log", message: "error 408")
-} catch {
-    print(error)
-}
+log(message: "error", to: "./errors")
 
