c02f285cda60ace33342968e605f9145cb5f9261
cham-s <cc.attou@gmail.com>
2019-01-16 22:41:58 +0100

    first version of c52

--- challenges/Challenge52/Sources/Challenge52/Challenge52.swift
+++ challenges/Challenge52/Sources/Challenge52/Challenge52.swift
@@ -1,3 +1,21 @@
-struct Challenge52 {
-    var text = "Hello, World!"
+import Foundation
+
+struct Gigasecond {
+    var description: String
+    
+    init?(from: String) {
+        let RFC3339DateFormatter = DateFormatter()
+        RFC3339DateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        RFC3339DateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        RFC3339DateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        
+        let date = RFC3339DateFormatter.date(from: from)
+        
+        if var date = date {
+            date.addTimeInterval(1_000_000_000)
+            description = RFC3339DateFormatter.string(from: date)
+        } else {
+            return nil
+        }
+    }
 }
