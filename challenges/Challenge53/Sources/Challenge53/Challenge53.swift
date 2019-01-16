50e67a9017990d4256e37ac0ee035353f02aaa93
cham-s <cc.attou@gmail.com>
2019-01-16 23:31:46 +0100

    finished first version of c53

--- challenges/Challenge53/Sources/Challenge53/Challenge53.swift
+++ challenges/Challenge53/Sources/Challenge53/Challenge53.swift
@@ -1,1 +1,19 @@
+import Foundation
 
+class Squares {
+    var value: Int
+    lazy var squareOfSums: Int = {
+        let number = (1...value).reduce(0, +)
+        return number * number
+    }()
+    lazy var sumOfSquares: Int = {
+        return (1...value).reduce(0) { $0 + ($1 * $1) }
+    }()
+    lazy var differenceOfSquares: Int = {
+        return squareOfSums - sumOfSquares
+    }()
+    
+    init(_ value: Int) {
+        self.value = value
+    }
+}
