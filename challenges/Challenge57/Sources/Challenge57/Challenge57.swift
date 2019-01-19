eb1381017ede8109c80da32e3457fcd06bc6a4a7
cham-s <cc.attou@gmail.com>
2019-01-19 20:22:57 +0100

    improved to avoid nested ifs

--- challenges/Challenge57/Sources/Challenge57/Challenge57.swift
+++ challenges/Challenge57/Sources/Challenge57/Challenge57.swift
@@ -1,30 +1,34 @@
 import Foundation
 
 struct Bob {
-    static func hey(_ input: String) -> String {
-        let trimmedInput = input.trimmingCharacters(in: .whitespaces)
-        guard trimmedInput.isEmpty == false else {
+    static func hey(_ input: String) -> String {        
+        switch input {
+        case let string where string.hasNoLetter:
             return "Fine. Be that way!"
-        }
-        if input.isAllUppercased {
-            if input.rangeOfCharacter(from: .letters) == nil {
-                if input.last! == "?" {
-                    return "Sure."
-                }
-                return "Whatever."
-            }
+            case let string where string.isAllUppercased && string.hasLetter:
             return "Whoa, chill out!"
-        }
-        if input.last! == "?" {
+        case let string where string.isInterrogative:
             return "Sure."
+        default:
+            return "Whatever."
         }
-        
-        return "Whatever."
     }
 }
 
 extension String {
     var isAllUppercased: Bool {
         return self.uppercased() == self
+    }
+    
+    var isInterrogative: Bool {
+        return self.hasSuffix("?")
+    }
+    
+    var hasLetter: Bool {
+        return (self.rangeOfCharacter(from: .letters)) != nil
+    }
+    
+    var hasNoLetter: Bool {
+        return isEmpty || self.trimmingCharacters(in: .whitespaces).isEmpty
     }
 }
