2d71d8671c9233dd0ee70689c4754af37b367954
cham-s <cc.attou@gmail.com>
2019-01-21 22:49:37 +0100

    c60 so far

--- challenges/Challenge60/Sources/Challenge60/Challenge60.swift
+++ challenges/Challenge60/Sources/Challenge60/Challenge60.swift
@@ -1,3 +1,38 @@
-struct Challenge60 {
-    var text = "Hello, World!"
+struct ListOps {
+    static func append(_ destination: [Int], _ source: [Int]) -> [Int] {
+        return destination + source
+    }
+    
+    static func concat(_ destination: [Int], _ list: [Int]...) -> [Int] {
+        var array = destination
+        for item in list {
+            array = ListOps.append(destination, item)
+        }
+        return array
+    }
+    
+    static func filter(_ source: [Int], predicate: (Int) -> Bool) -> [Int] {
+        var array: [Int] = []
+        for item in source where predicate(item) == true {
+            array = ListOps.append(array, [item])
+        }
+        return array
+        
+    }
+    
+    static func length(_ source: [Int]) -> Int {
+        var count = 0
+        for _ in source {
+            count += 1
+        }
+        return count
+    }
+    
+    static func map(_ source: [Int], _ transform: (Int) -> Int) -> [Int] {
+        var array: [Int] = []
+        for item in source {
+            array = ListOps.append(array, [transform(item)])
+        }
+        
+    }
 }
