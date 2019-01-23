48024dad449088ddb395cb7459b469fdc834e8fa
cham-s <cc.attou@gmail.com>
2019-01-23 22:25:49 +0100

    c62 so far

--- challenges/Challenge62/Sources/Challenge62/Challenge62.swift
+++ challenges/Challenge62/Sources/Challenge62/Challenge62.swift
@@ -4,19 +4,12 @@ struct Clock: Equatable {
     var hours: Int = 0
     var minutes: Int = 0
     var description: String {
-        let totalMinutes = 60 * hours + minutes
-        var h = (totalMinutes / 60) % 24
-        if h < 0 {
-            h = h + 24
-        }
-        
-        let m = totalMinutes % 60
-        return String(format: "%02d:%02d", h, m)
+        return String(format: "%02d:%02d", hours, minutes)
     }
     
     init(hours: Int, minutes: Int = 0) {
-        self.hours = hours
-        self.minutes = minutes
+        updateMinutes(withMinutes: minutes)
+        updateHours(withHours: hours)
     }
     
     mutating func add(minutes: Int) {
@@ -27,8 +20,24 @@ struct Clock: Equatable {
         self.minutes -= minutes
     }
     
-    static func  == (lhs: Clock, rhs: Clock) -> Bool {
-        return lhs.minutes == rhs.minutes &&
-              lhs.hours == rhs.hours
+    mutating func updateHours(withHours: Int) {
+        var h = withHours % 24
+        if h < 0 {
+            h += 24
+        }
+        hours = (hours + h) % 24
+    }
+    
+    mutating func updateMinutes(withMinutes: Int) {
+        let h = withMinutes / 60
+        var m = withMinutes % 60
+        if h > 0 {
+            updateHours(withHours: h)
+        }
+        
+        if m < 0 {
+            m += 60
+        }
+        minutes = m
     }
 }
