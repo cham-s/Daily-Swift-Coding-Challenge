309ecc0e8ca0153ce87e1d8e33ff4a00712e43d7
cham-s <cc.attou@gmail.com>
2019-02-02 23:41:37 +0100

    c69 so far

--- challenges/Challenge69/Sources/Challenge69/Challenge69.swift
+++ challenges/Challenge69/Sources/Challenge69/Challenge69.swift
@@ -1,3 +1,42 @@
-struct Challenge69 {
-    var text = "Hello, World!"
+struct Sieve {
+    var limit = 0
+    
+    var primes: [Int] {
+        guard limit > 2 else {
+            return []
+        }
+        
+        var primes: [Int] = []
+        var numbers = (2...limit).map {  (number: $0, marked: false)  }
+        var primeIndex = 0
+        var numberIndex = 1
+        
+        while numbers[primeIndex].number != limit {
+            guard !numbers[numberIndex].marked else {
+                primeIndex += 1
+                numberIndex = primeIndex + 1
+                continue
+            }
+            primes.append(numbers[primeIndex].number)
+            while numbers[numberIndex].number != limit {
+                guard !numbers[numberIndex].marked else {
+                    numberIndex += 1
+                    continue
+                }
+                if numbers[numberIndex].number % numbers[primeIndex].number == 0 {
+                    numbers[numberIndex].marked = true
+                }
+                numberIndex += 1
+            }
+            primeIndex += 1
+            numberIndex = primeIndex + 1
+        }
+        
+        return primes
+    }
+    
+    init(_ limit: Int) {
+        self.limit = limit
+    }
 }
+
