b6ffb5af113f51422699927d191bf224c9598b70
cham-s <cc.attou@gmail.com>
2019-02-02 02:54:18 +0100

    finished 1st ver of c68

--- challenges/Challenge68/Sources/Challenge68/Challenge68.swift
+++ challenges/Challenge68/Sources/Challenge68/Challenge68.swift
@@ -1,3 +1,30 @@
-struct Challenge68 {
-    var text = "Hello, World!"
+
+extension Collection {
+    func discard(_ predicate: (Element) throws -> Bool)
+        rethrows -> [Element] {
+        var array = ContiguousArray<Element>()
+        var iterator = self.makeIterator()
+        
+        while let element = iterator.next() {
+            if try predicate(element) == false {
+                array.append(element)
+            }
+        }
+        
+        return Array(array)
+    }
+    
+    func keep(_ predicate: (Element) throws -> Bool)
+        rethrows -> [Element] {
+            var array = ContiguousArray<Element>()
+            var iterator = self.makeIterator()
+            
+            while let element = iterator.next() {
+                if try predicate(element) {
+                    array.append(element)
+                }
+            }
+            
+            return Array(array)
+    }
 }
