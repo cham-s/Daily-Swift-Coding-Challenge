c1afa40e1db4b20c7447455ba8749aab58407e32
cham-s <cc.attou@gmail.com>
2019-02-04 08:48:56 +0100

    working version of c71 but ugly

--- challenges/Challenge71/Sources/Challenge71/Challenge71.swift
+++ challenges/Challenge71/Sources/Challenge71/Challenge71.swift
@@ -1,52 +1,44 @@
 import Foundation
 
-//enum Number {
-//    case withCountryCode
-//    case withoutCountryCode
-//    case invalid
-//
-//    init(_ count: String) {
-//        switch count {
-//        case 10:
-//            self = .withoutCountryCode
-//        case 11:
-//            self = .withCountryCode
-//        default:
-//            self = .invalid
-//        }
-//    }
-//}
-
 struct PhoneNumber {
     var areaCode = "000"
     var exchangeCode = "000"
     var subscriber = "0000"
     var number = "0000000000"
     
-    init(number: String) {
+    init(_ number: String) {
         var filtered = number.filter {
             String($0).rangeOfCharacter(from: CharacterSet.decimalDigits) != nil
         }
         guard filtered.count == 11 || filtered.count == 10 else {
             return
         }
-        
-        _ = filtered.count == 11 ? String(filtered.removeFirst()) : ""
+        if filtered.count == 11 {
+            if String(filtered.removeFirst()) != "1" {
+                return
+            }
+        }
         let endArea = filtered.index(filtered.startIndex, offsetBy: 3)
         let endtExchange = filtered.index(endArea, offsetBy: 3)
         let area = String(filtered[filtered.startIndex..<endArea])
-        let firstNumber = Int(String(area[area.startIndex]))!
+        var firstNumber = Int(String(area[area.startIndex]))!
         guard firstNumber > 1 || firstNumber < 10 else {
-            countryCode = ""
             return
         }
-        exchangeCode = String(filtered[endArea..<endtExchange])
-        subscriber = String(filtered[endtExchange...filtered.endIndex])
+        let exchange = filtered[endArea..<endtExchange]
+        firstNumber = Int(String(exchange[exchange.startIndex]))!
+        guard firstNumber > 1 || firstNumber < 10 else {
+            return
+        }
+        self.areaCode = String(area)
+        self.exchangeCode = String(exchange)
+        self.subscriber = String(filtered[endtExchange..<filtered.endIndex])
         self.number = filtered
     }
-    
-//    mutating private func check(number: String) -> Bool {
-//
-//    }
 }
 
+extension PhoneNumber: CustomStringConvertible {
+    var description: String {
+        return "(\(areaCode)) \(exchangeCode)-\(subscriber)"
+    }
+}
