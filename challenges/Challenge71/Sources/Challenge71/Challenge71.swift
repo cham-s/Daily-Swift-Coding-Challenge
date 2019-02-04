00783c3dd1c3804ab9d706edd7270782438f09d7
cham-s <cc.attou@gmail.com>
2019-02-04 20:53:28 +0100

    trying something new

--- challenges/Challenge71/Sources/Challenge71/Challenge71.swift
+++ challenges/Challenge71/Sources/Challenge71/Challenge71.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+struct Code {
+    var area = "000"
+    var exchange = "000"
+    var subscriber = "0000"
+    var number = "0000000000"
+}
+
 struct PhoneNumber {
     var areaCode = "000"
     var exchangeCode = "000"
@@ -10,32 +17,33 @@ struct PhoneNumber {
         var filtered = number.filter {
             String($0).rangeOfCharacter(from: CharacterSet.decimalDigits) != nil
         }
-        guard filtered.count == 11 || filtered.count == 10 else {
-            return
-        }
+        guard 10...11 ~= filtered.count else { return }
+        
         if filtered.count == 11 {
             if String(filtered.removeFirst()) != "1" {
                 return
             }
         }
+        
         let endArea = filtered.index(filtered.startIndex, offsetBy: 3)
         let endtExchange = filtered.index(endArea, offsetBy: 3)
         let area = String(filtered[filtered.startIndex..<endArea])
         var firstNumber = Int(String(area[area.startIndex]))!
-        guard firstNumber > 1 || firstNumber < 10 else {
-            return
-        }
+        guard 0...9 ~= firstNumber  else { return }
+        
         let exchange = filtered[endArea..<endtExchange]
         firstNumber = Int(String(exchange[exchange.startIndex]))!
-        guard firstNumber > 1 || firstNumber < 10 else {
-            return
-        }
+        guard 0...9 ~= firstNumber  else { return }
+        
         self.areaCode = String(area)
         self.exchangeCode = String(exchange)
         self.subscriber = String(filtered[endtExchange..<filtered.endIndex])
         self.number = filtered
     }
+    
 }
+
+
 
 extension PhoneNumber: CustomStringConvertible {
     var description: String {
