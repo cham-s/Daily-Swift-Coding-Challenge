99f235f0074e83e3d6cfdb239de9a4e1756abedb
cham-s <cc.attou@gmail.com>
2019-02-04 23:49:01 +0100

    added script for c71

--- scripts/test_challenges.sh
+++ scripts/test_challenges.sh
@@ -12,6 +12,8 @@ declare -a valid=("Challenge01" "Challenge02" "Challenge03" "Challenge04" \
 	"Challenge55" "Challenge56" "Challenge57" "Challenge58" "Challenge59" \
 	"Challenge60" "Challenge61" "Challenge63" "Challenge64" "Challenge65" \
 	"Challenge66" "Challenge67" "Challenge68" "Challenge69" "Challenge70" \
+	"Challenge71"
+
 	)
 
 # TODO: more dynamic approach with a filter for testable packages.
