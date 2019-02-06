bb43604dec4f72ca65adc7390df790586ed5582d
cham-s <cc.attou@gmail.com>
2019-02-06 08:56:50 +0100

    c75 so far

--- challenges/Challenge75/Sources/Challenge75/Challenge75.swift
+++ challenges/Challenge75/Sources/Challenge75/Challenge75.swift
@@ -1,3 +1,30 @@
-struct Challenge75 {
-    var text = "Hello, World!"
+enum Classifier {
+    case equal
+    case superlist
+    case sublist
+    case unequal
+    
+    init (first: [Int], second: [Int]) {
+        guard first != second else {
+            self = .equal
+            return
+        }
+        let firstSet = Set(first)
+        let secondSet = Set(second)
+        if firstSet.isSubset(of: secondSet) {
+            self = .sublist
+            return
+        }
+        if firstSet.isSuperset(of: secondSet) {
+            self = .superlist
+        } else {
+            self = .unequal
+        }
+    }
+}
+
+
+
+func classifier(listOne: [Int], listTwo: [Int]) -> Classifier {
+    return Classifier(first: listOne, second: listTwo)
 }
