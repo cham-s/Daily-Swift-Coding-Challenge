7d885a60976c1b6b5448fe8d105e3864277652a9
cham-s <cc.attou@gmail.com>
2019-02-07 05:55:32 +0100

    1st version of c75

--- challenges/Challenge75/Sources/Challenge75/Challenge75.swift
+++ challenges/Challenge75/Sources/Challenge75/Challenge75.swift
@@ -10,7 +10,7 @@ enum Classifier {
             self = .equal
         case (let one, let two) where one.isSuperList(of: two):
             self = .superlist
-        case (let one, let two) where two.isSuperList(of: one):
+        case (let one, let two) where one.isSubList(of: two):
             self = .sublist
         default:
             self = .unequal
@@ -23,31 +23,51 @@ extension Array where Element: Comparable {
         return self == list
     }
     
+    func isSubList(of list: [Element]) -> Bool {
+        guard count != list.count else { return false }
+        if isEmpty && !list.isEmpty {
+            return true
+        }
+        
+        var selfIndex = 0
+        var listIndex = 0
+        var lastIndex = 0
+        while listIndex < list.count {
+            while self[selfIndex] == list[lastIndex] {
+                selfIndex += 1
+                lastIndex += 1
+                if selfIndex == count { return true }
+                if lastIndex == list.count { return false }
+            }
+            listIndex += 1
+            lastIndex = listIndex
+            selfIndex = 0
+        }
+        return false
+    }
+    
     func isSuperList(of list: [Element]) -> Bool {
+        guard count != list.count else { return false }
         if !isEmpty && list.isEmpty {
             return true
         }
         
         var selfIndex = 0
         var listIndex = 0
-        while true {
-            if self[selfIndex] == list[listIndex] {
-                selfIndex += 1
+        var lastIndex = 0
+        while selfIndex < count {
+            while self[lastIndex] == list[listIndex] {
                 listIndex += 1
-                if listIndex == list.count - 1 {
-                    return true
-                }
-                if selfIndex == count - 1 {
-                    return false
-                }
-            } else {
-                listIndex = 0
-                selfIndex += 1
-                if selfIndex == count - 1 {
-                    return false
-                }
+                lastIndex += 1
+                if listIndex == list.count { return true }
+                if lastIndex == count { return false }
             }
+            listIndex = 0
+            selfIndex += 1
+            lastIndex = selfIndex
+            
         }
+        return false
     }
 }
 
