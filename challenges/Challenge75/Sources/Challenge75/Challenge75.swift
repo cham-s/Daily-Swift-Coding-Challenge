b37ab0f89a9abc73e5719c741c40b5cc0e7b6a99
cham-s <cc.attou@gmail.com>
2019-02-07 03:49:03 +0100

    some update not ready yet

--- challenges/Challenge75/Sources/Challenge75/Challenge75.swift
+++ challenges/Challenge75/Sources/Challenge75/Challenge75.swift
@@ -5,25 +5,51 @@ enum Classifier {
     case unequal
     
     init (first: [Int], second: [Int]) {
-        guard first != second else {
+        switch (first, second) {
+        case (let one, let two) where one.isEqual(to: two):
             self = .equal
-            return
-        }
-        let firstSet = Set(first)
-        let secondSet = Set(second)
-        if firstSet.isSubset(of: secondSet) {
-            self = .sublist
-            return
-        }
-        if firstSet.isSuperset(of: secondSet) {
+        case (let one, let two) where one.isSuperList(of: two):
             self = .superlist
-        } else {
+        case (let one, let two) where two.isSuperList(of: one):
+            self = .sublist
+        default:
             self = .unequal
         }
     }
 }
 
-
+extension Array where Element: Comparable {
+    func isEqual(to list: [Element]) -> Bool {
+        return self == list
+    }
+    
+    func isSuperList(of list: [Element]) -> Bool {
+        if !isEmpty && list.isEmpty {
+            return true
+        }
+        
+        var selfIndex = 0
+        var listIndex = 0
+        while true {
+            if self[selfIndex] == list[listIndex] {
+                selfIndex += 1
+                listIndex += 1
+                if listIndex == list.count - 1 {
+                    return true
+                }
+                if selfIndex == count - 1 {
+                    return false
+                }
+            } else {
+                listIndex = 0
+                selfIndex += 1
+                if selfIndex == count - 1 {
+                    return false
+                }
+            }
+        }
+    }
+}
 
 func classifier(listOne: [Int], listTwo: [Int]) -> Classifier {
     return Classifier(first: listOne, second: listTwo)
