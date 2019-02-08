54c2423a0666cdb3b7f6f6c5883895157018ce37
cham-s <cc.attou@gmail.com>
2019-02-08 02:19:27 +0100

    c76 so faor dico declaration

--- challenges/Challenge76/Sources/Challenge76/Challenge76.swift
+++ challenges/Challenge76/Sources/Challenge76/Challenge76.swift
@@ -1,1 +1,14 @@
-
+struct SecretHandshake {
+    let secrets: [UInt16: String] = [
+        0x1: "wink",
+        0x10: "double blink",
+        0x100: "close your eyes",
+        0x1000: "jump"
+    ]
+    
+    var result: [String] = []
+    
+    init(_ input: UInt16) {
+       
+    }
+}
