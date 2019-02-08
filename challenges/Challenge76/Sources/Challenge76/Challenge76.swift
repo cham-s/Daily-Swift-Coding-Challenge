c6a46c582a56f992f741d1d3fb0702cf604e42de
cham-s <cc.attou@gmail.com>
2019-02-08 03:48:24 +0100

    finished 1st ver of c76

--- challenges/Challenge76/Sources/Challenge76/Challenge76.swift
+++ challenges/Challenge76/Sources/Challenge76/Challenge76.swift
@@ -1,14 +1,20 @@
+func &<T: UnsignedInteger>(left: T, right: T) ->  Bool {
+    return left & right != 0
+}
+
 struct SecretHandshake {
-    let secrets: [UInt16: String] = [
-        0x1: "wink",
-        0x10: "double blink",
-        0x100: "close your eyes",
-        0x1000: "jump"
+    private let secrets: [(key: UInt16, value: String)] = [
+        (key: 1, value: "wink"),
+        (key: 2, value: "double blink"),
+        (key: 4, value: "close your eyes"),
+        (key: 8, value: "jump")
     ]
+    private let reversable: UInt16 = 16
     
-    var result: [String] = []
+    var commands: [String] = []
     
     init(_ input: UInt16) {
-       
+        commands = secrets.filter { input & $0.key }.map { $0.value }
+        if input & reversable { commands.reverse() }
     }
 }
