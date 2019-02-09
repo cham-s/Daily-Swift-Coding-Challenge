752f5c7bfa79b64936d90bf7d03c743b798402f0
cham-s <cc.attou@gmail.com>
2019-02-09 10:03:40 +0100

    2 tests left

--- challenges/Challenge79/Sources/Challenge79/Challenge79.swift
+++ challenges/Challenge79/Sources/Challenge79/Challenge79.swift
@@ -1,1 +1,21 @@
+import Foundation
+
+struct IsbnVerifier {
+    
+    static func isValid(_ input: String) -> Bool {
+        
+        let validSet = CharacterSet(charactersIn: "0123456789X")
+        guard input.rangeOfCharacter(from: validSet) != nil else { return false }
+        
+        let striped = input.components(separatedBy: "-").joined()
+        guard striped.count == 10 else { return false }
+        
+        let numbers: [Int] = striped.map { Int(String($0)) ?? 10 }
+        var result = 0
+        for (index, value) in (1...10).reversed().enumerated() {
+            result += numbers[index] * value
+        }
+        return result % 11 == 0
+    }
+}
 
