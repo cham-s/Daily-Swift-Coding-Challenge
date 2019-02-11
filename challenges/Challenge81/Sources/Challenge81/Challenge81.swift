1207af582fc024addabf0a2659ddaf84f00ac11b
cham-s <cc.attou@gmail.com>
2019-02-11 04:30:49 +0100

    first working version of c81

--- challenges/Challenge81/Sources/Challenge81/Challenge81.swift
+++ challenges/Challenge81/Sources/Challenge81/Challenge81.swift
@@ -3,7 +3,18 @@ enum CircularBufferError: Error {
     case bufferFull
 }
 
-class CircularBuffer<T> {
+class CircularBuffer<T>: CustomStringConvertible {
+    var description: String {
+        var output = ""
+        _buffer.forEach {
+            if let e = $0 {
+                output += "[ \(e) ]"
+            } else {
+                output += "[  ]"
+            }
+        }
+        return output
+    }
     
     private struct Index {
         var last = 0
@@ -15,37 +26,38 @@ class CircularBuffer<T> {
         }
     }
     
-    private var _buffer: ContiguousArray<T> = []
+    private var _buffer: ContiguousArray<T?> = []
     private var _capacity: Int
     private var _used = 0
     private var _indices = Index()
-    private var _buffuerIsFull: Bool { return _used == _capacity }
+    private var _bufferIsFull: Bool { return _used == _capacity }
     private var _bufferIsEmpty: Bool { return _used == 0 }
     
     init(capacity: Int) {
         _buffer.reserveCapacity(capacity)
         _capacity = capacity
+        for _ in 0..<capacity { _buffer.append(nil) }
     }
     
     public func write(_ data: T) throws {
-        guard _buffuerIsFull == false else {
+        guard _bufferIsFull == false else {
             throw CircularBufferError.bufferFull
         }
         
-        let nextIndex = (_indices.last) % _capacity
-        _buffer[nextIndex] = data
-        _indices.last += 1
+        _buffer[_indices.last] = data
+        _indices.last = (_indices.last + 1) % _capacity
         _used += 1
     }
     
     public func read() throws -> T {
         guard _bufferIsEmpty == false else {
-            throw CircularBufferError.bufferFull
+            throw CircularBufferError.bufferEmpty
         }
         
         let data = _buffer[_indices.oldest]
-        _indices.oldest += 1
-        return data
+        _used -= 1
+        _indices.oldest = (_indices.oldest + 1) % _capacity
+        return data!
     }
     
     public func clear() {
@@ -55,6 +67,9 @@ class CircularBuffer<T> {
     
     public func overwrite(_ withData: T)  {
         _buffer[_indices.oldest] = withData
-        _indices.oldest += 1
+        _indices.oldest = (_indices.oldest + 1) % _capacity
+        if _bufferIsFull == false {
+            _used += 1
+        }
     }
 }
