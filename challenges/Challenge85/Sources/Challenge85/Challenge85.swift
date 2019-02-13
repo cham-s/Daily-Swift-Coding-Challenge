fcef5b29a1141168c4eb78f019599dfe089366d6
cham-s <cc.attou@gmail.com>
2019-02-13 04:43:52 +0100

    first version of c85

--- challenges/Challenge85/Sources/Challenge85/Challenge85.swift
+++ challenges/Challenge85/Sources/Challenge85/Challenge85.swift
@@ -1,3 +1,22 @@
-struct Challenge85 {
-    var text = "Hello, World!"
+import Foundation
+
+struct WordCount {
+    private var wordCount: [String: Int] = [:]
+    
+    init(words: String) {
+        populateWordCount(str: words)
+    }
+    
+    func count() -> [String: Int] {
+        return wordCount
+    }
+    
+    private mutating func populateWordCount(str: String) {
+        guard str.isEmpty == false else { return }
+        
+        let separator = CharacterSet.alphanumerics.inverted
+        let components = str.components(separatedBy: separator)
+                        .filter { !$0.isEmpty }.map { $0.lowercased() }
+        components.forEach { wordCount[$0, default: 0] += 1 }
+    }
 }
