77faacfa0ddbf69d6194bbef9887a3a14280383d
cham-s <cc.attou@gmail.com>
2019-02-13 03:35:47 +0100

    first version of c84

--- challenges/Challenge84/Sources/Challenge84/Challenge84.swift
+++ challenges/Challenge84/Sources/Challenge84/Challenge84.swift
@@ -1,3 +1,27 @@
-struct Challenge84 {
-    var text = "Hello, World!"
+
+func &<T: UnsignedInteger>(lhs: T, rhs: T) -> Bool {
+    return lhs & rhs != 0
+}
+
+enum Allergy: UInt, CaseIterable {
+    case eggs = 1
+    case peanuts =  2
+    case shellfish = 4
+    case strawberries = 8
+    case tomatoes = 16
+    case chocolate = 32
+    case pollen = 64
+    case cats = 128
+}
+
+struct Allergies {
+    var score: UInt
+    
+    init(_ score: UInt) {
+        self.score = score
+    }
+    
+    func hasAllergy(_ allergy: Allergy) -> Bool {
+        return allergy.rawValue & score
+    }
 }
