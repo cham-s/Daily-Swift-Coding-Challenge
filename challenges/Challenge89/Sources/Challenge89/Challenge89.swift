924adcad4968eca1383ca1dd91c2ab37380f3ad2
cham-s <cc.attou@gmail.com>
2019-02-17 06:51:30 +0100

    c89 so far one tricky test left

--- challenges/Challenge89/Sources/Challenge89/Challenge89.swift
+++ challenges/Challenge89/Sources/Challenge89/Challenge89.swift
@@ -1,3 +1,13 @@
-struct Challenge89 {
-    var text = "Hello, World!"
+import Foundation
+
+struct Acronym {
+    static func abbreviate(_ input: String) -> String {
+        guard input.isEmpty == false else { return "" }
+        let nonLetterSet = CharacterSet(
+                           charactersIn: "abcdefghijklmnopqrstuvwxyz").inverted
+        return String(input.lowercased()
+                    .components(separatedBy: nonLetterSet)
+                    .filter { !$0.isEmpty }
+                    .map { $0.first! }).uppercased()
+    }
 }
