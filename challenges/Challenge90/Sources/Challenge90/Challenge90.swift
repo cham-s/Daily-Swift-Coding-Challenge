02e28dd3ae096fed6aa1c5d6d1ce378df9275fa3
cham-s <cc.attou@gmail.com>
2019-02-18 03:13:58 +0100

    first version of c90

--- challenges/Challenge90/Sources/Challenge90/Challenge90.swift
+++ challenges/Challenge90/Sources/Challenge90/Challenge90.swift
@@ -1,1 +1,16 @@
-
+struct Anagram {
+    
+    public var word: String
+    
+    init(word: String) {
+        self.word = word
+    }
+    
+    public func match(_ words: [String]) -> [String] {
+        guard words.isEmpty == false else { return [] }
+        return words.filter {
+            $0.lowercased() != word.lowercased() &&
+            $0.lowercased().sorted() == word.lowercased().sorted()
+        }
+    }
+}
