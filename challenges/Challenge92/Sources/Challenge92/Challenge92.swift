be03227def31bc0dc695453ed7fab241c49f8699
cham-s <cc.attou@gmail.com>
2019-02-18 04:08:52 +0100

    unitests for c91

---
+++ challenges/Challenge92/Sources/Challenge92/Challenge92.swift
@@ -0,0 +1,3 @@
+struct Challenge92 {
+    var text = "Hello, World!"
+}
---
+++ challenges/Challenge92/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import Challenge92Tests
+
+var tests = [XCTestCaseEntry]()
+tests += Challenge92Tests.allTests()
+XCTMain(tests)
