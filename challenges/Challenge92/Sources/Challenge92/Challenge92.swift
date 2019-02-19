d5813bc04b741d0ed77afafc94efa6af33651f80
cham-s <cc.attou@gmail.com>
2019-02-19 05:56:33 +0100

    c92 so far

--- challenges/Challenge92/Sources/Challenge92/Challenge92.swift
+++ challenges/Challenge92/Sources/Challenge92/Challenge92.swift
@@ -1,3 +1,42 @@
-struct Challenge92 {
-    var text = "Hello, World!"
+class Element<T> {
+    var value: T?
+    var next: Element?
+    private var start: Element?
+    
+    init(_ value: T? = nil, _ next: Element? = nil) {
+        self.value = value
+        self.next = next
+        start = next
+    }
+    
+    func toArray() -> [T] {
+        var array: [T] = []
+        if let val = value {
+            array.append(val)
+        }
+        var tmp = next
+        while let nextNode = tmp {
+            if let val = nextNode.value {
+                array.append(val)
+                tmp = tmp?.next
+            }
+        }
+        return array
+    }
+    
+    func reverseElements() -> Element {
+        start = self.next
+        link(previous: self)
+        start = nil
+        return next!
+    }
+    
+    private func link(previous element: Element) {
+        
+        if let nextElement = element.next {
+            link(previous: nextElement)
+            next?.next = element
+        }
+        next = element
+    }
 }
