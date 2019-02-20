4675de9a686481f493e09c067cf698b7730d1b48
cham-s <cc.attou@gmail.com>
2019-02-20 05:43:50 +0100

    c93 so far

--- challenges/Challenge93/Sources/Challenge93/Challenge93.swift
+++ challenges/Challenge93/Sources/Challenge93/Challenge93.swift
@@ -1,3 +1,30 @@
-struct Challenge93 {
-    var text = "Hello, World!"
+struct Matrix {
+    var strMatrix: String
+    var rows: [[Int]] {
+        return strMatrix.split(separator: "\n")
+            .map { $0.split(separator: " ")
+                .map { Int(String($0))! } }
+    }
+    var columns: [[Int]] {
+        let rows = strMatrix.split(separator: "\n")
+        var array: [[Int]] = []
+        
+        let count = rows.first!.count
+        
+        for col in 0..<count {
+            var numbers: [Int] = []
+            for row in rows {
+                if let number = Int(String(Array(row)[col])) {
+                    numbers.append(number)
+                }
+            }
+            array.append(numbers)
+        }
+        return array
+    }
+    
+    init(_ strMatrix: String) {
+        self.strMatrix = strMatrix
+    }
 }
+
