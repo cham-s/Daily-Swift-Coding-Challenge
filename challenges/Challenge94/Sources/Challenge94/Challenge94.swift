1c7741541ac071c8e082446c4dc6eb492a824494
cham-s <cc.attou@gmail.com>
2019-02-21 02:20:34 +0100

    added c94

---
+++ challenges/Challenge94/Sources/Challenge94/Challenge94.swift
@@ -0,0 +1,3 @@
+struct Challenge94 {
+    var text = "Hello, World!"
+}
---
+++ challenges/Challenge94/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import Challenge94Tests
+
+var tests = [XCTestCaseEntry]()
+tests += Challenge94Tests.allTests()
+XCTMain(tests)
