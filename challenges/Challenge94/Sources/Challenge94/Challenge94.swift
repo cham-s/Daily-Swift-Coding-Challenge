f0fa9a2bbc40265b58f56358d72703fa5d63ae31
cham-s <cc.attou@gmail.com>
2019-02-22 03:46:49 +0100

    working on error

--- challenges/Challenge94/Sources/Challenge94/Challenge94.swift
+++ challenges/Challenge94/Sources/Challenge94/Challenge94.swift
@@ -1,7 +1,33 @@
+import Foundation
+
+enum BaseError: Error {
+    case negativeDigit
+    case invalidPositiveDigit
+    case invalidInputBase
+    case invalidOutputBase
+}
+
 struct Base {
     static func outputDigits(inputBase: Int,
                              inputDigits: [Int],
-                             outputBase: Int) -> [Int] {
-        let result
+                             outputBase: Int) throws -> [Int] {
+        guard inputDigits.isEmpty == false else {
+            throw BaseError.invalidInputBase
+        }
+        guard (inputDigits.first { $0 < 0 }) == nil else {
+            throw BaseError.negativeDigit
+        }
+        guard (inputDigits.first { $0 > inputBase - 1 }) == nil else {
+            throw BaseError.invalidPositiveDigit
+        }
+        let zipped = zip(inputDigits, (0..<inputDigits.count).reversed())
+        var result = zipped.map { $0 *  Int(pow(Double(inputBase), Double($1))) }
+                     .reduce(0, +)
+        var array: [Int] = []
+        while result != 0 {
+            array.append(result % outputBase)
+            result /= outputBase
+        }
+        return Array(array.reversed())
     }
 }
