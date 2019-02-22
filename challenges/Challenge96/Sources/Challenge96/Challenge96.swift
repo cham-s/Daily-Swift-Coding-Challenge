e2f4dd6807a4f0ef6d77481a8419bedaa4546ca8
cham-s <cc.attou@gmail.com>
2019-02-23 00:40:30 +0100

    c96 so far

--- challenges/Challenge96/Sources/Challenge96/Challenge96.swift
+++ challenges/Challenge96/Sources/Challenge96/Challenge96.swift
@@ -1,3 +1,5 @@
 struct House {
-    
+    static let verbs = ["lay", "ate", "killed", "worried", "tossed", "milked",
+                        "kissed", "married", "woke", "kept", "belonged"]
+    static let nouns = [""]
 }
