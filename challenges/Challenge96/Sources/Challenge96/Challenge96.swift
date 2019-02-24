055b784776b8f09744035a63d1b53ab51df8a2c1
cham-s <cc.attou@gmail.com>
2019-02-24 04:34:40 +0100

    c97 so far

--- challenges/Challenge96/Sources/Challenge96/Challenge96.swift
+++ challenges/Challenge96/Sources/Challenge96/Challenge96.swift
@@ -1,5 +1,9 @@
 struct House {
     static let verbs = ["lay", "ate", "killed", "worried", "tossed", "milked",
                         "kissed", "married", "woke", "kept", "belonged"]
+    static let articles = ["the", "that"]
     static let nouns = [""]
+    
+    static let firstPrase = "This is the horse and the hound and the horn"
+    static let lastPhrase = "that lay in the house that Jack built"
 }
--- challenges/Challenge97/Sources/Challenge97/Challenge97.swift
+++ challenges/Challenge97/Sources/Challenge97/Challenge97.swift
@@ -1,3 +1,38 @@
-struct Challenge97 {
-    var text = "Hello, World!"
+import Foundation
+struct Triplet {
+    var values: [Int] = []
+    var sum: Int {
+        return values.reduce(0, +)
+    }
+    
+    var product: Int {
+        return values.reduce(1, *)
+    }
+    
+    var isPythagorean: Bool {
+        return isValidTriplet(values[0], values[1], values[2])
+    }
+    
+    private func isValidTriplet(_ a: Int, _ b: Int, _ c: Int) -> Bool {
+        if sqrt(Double(a)) * sqrt(Double(b)) == sqrt(Double(c)) {
+            return true
+        }
+        if sqrt(Double(c)) * sqrt(Double(a)) == sqrt(Double(b)) {
+            return true
+        }
+        
+        if sqrt(Double(b)) * sqrt(Double(c)) == sqrt(Double(a)) {
+            return true
+        }
+        return false
+    }
+    
+    init(_ f: Int, _ s: Int, _ t: Int) {
+        values.append(contentsOf: [f, s, t])
+    }
+    
+    public func fromWhere(_ minFactor: Int = 0, maxFactor: Int) -> [Int] {
+        
+    }
+
 }
