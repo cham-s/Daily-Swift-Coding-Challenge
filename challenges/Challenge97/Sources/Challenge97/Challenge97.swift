f6414375971e1734c241b1e42da1f541ef6f7b95
cham-s <cc.attou@gmail.com>
2019-02-26 05:03:43 +0100

    changed fromWhere, still working on it [ci skip]

--- challenges/Challenge97/Sources/Challenge97/Challenge97.swift
+++ challenges/Challenge97/Sources/Challenge97/Challenge97.swift
@@ -11,42 +11,49 @@ struct Triplet {
     }
     
     var isPythagorean: Bool {
-        return Triplet.isValidTriplet(values) != nil
+        return Triplet.isValidTriplet(values)
     }
     
-    static private func isValidTriplet(_ input: [Int]) -> Int? {
-        guard input.count == 3 else { return nil }
+    static private func isValidTriplet(_ input: [Int]) -> Bool {
+        guard input.count == 3 else { return false }
         for i in 0..<input.count {
             let (a, b, c) = (input[i], input[(i + 1) % 3], input[(i + 2) % 3])
             let result = pow(Double(a), 2) + pow(Double(b), 2)
             if result == pow(Double(c), 2) {
-                return input.reduce(1, *)
+                return true
             }
         }
-        return nil
+        return false
     }
     
     init(_ f: Int, _ s: Int, _ t: Int) {
         values.append(contentsOf: [f, s, t])
     }
     
-    static public func fromWhere(_ minFactor: Int = 1, maxFactor: Int) -> [Int] {
+    static public func fromWhere(_ minFactor: Int = 1, maxFactor: Int) -> [Triplet] {
         let range = Array(minFactor...maxFactor)
         guard range.count >= 3 else { return [] }
-        var array: [Int] = []
-        var count = 0
-        for a in minFactor...maxFactor {
-            for b in minFactor + 1...maxFactor {
-                for c in minFactor + 3...maxFactor {
-                    count += 1
-                    let input = [a, b, c]
-                    print(input)
-                    if let result = isValidTriplet(input) {
-                        array.append(result)
+        var array: [Triplet] = []
+        var a = minFactor
+        var b = minFactor + 1
+        var c = minFactor + 2
+        
+        while a <= maxFactor {
+            b = a + 1
+            while b <= maxFactor {
+                c = b + 1
+                while c <= maxFactor {
+                    if isValidTriplet([a, b, c]) {
+                        array.append(Triplet(a, b, c))
                     }
+                    c += 1
                 }
+                b += 1
             }
+            a += 1
         }
         return array
     }
 }
+
+
