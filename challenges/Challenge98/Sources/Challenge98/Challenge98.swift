da216ea275254b7d3851046d72e7f29ef433fd99
cham-s <cc.attou@gmail.com>
2019-02-27 05:10:17 +0100

    finished cols computed property [ci skip]

--- challenges/Challenge98/Sources/Challenge98/Challenge98.swift
+++ challenges/Challenge98/Sources/Challenge98/Challenge98.swift
@@ -7,6 +7,12 @@ struct SaddlePointsMatrix {
             .map { $0.split(separator: " ").map { Int($0)! } }
     }
     
+    var columns: [[Int]] {
+        let count = rows[0].count
+        return (0..<count).reduce([]) { partial, index in
+            partial + [ rows.map { $0[index] } ]
+        }
+    }
     
     init(_ matrix: String) {
         matrixStr = matrix
