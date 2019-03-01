febabe23efe02a0dab4acf4938a4493370415c09
cham-s <cc.attou@gmail.com>
2019-03-01 21:09:55 +0100

    c99 so far

--- challenges/Challenge99/Sources/Challenge99/Challenge99.swift
+++ challenges/Challenge99/Sources/Challenge99/Challenge99.swift
@@ -1,3 +1,20 @@
-struct Challenge99 {
-    var text = "Hello, World!"
+struct PascalsTriangle {
+    
+}
+
+
+let count = 4
+var array: [[Int]] = []
+array.reserveCapacity(count)
+for size in 1..<count {
+    var sub: [Int] = []
+    sub.reserveCapacity(size)
+    for position in sub.indices {
+        guard sub.count == 1 else {
+            sub[position] = 1
+            continue
+        }
+        guard
+            sub[position] = array[size - 1][position]
+    }
 }
