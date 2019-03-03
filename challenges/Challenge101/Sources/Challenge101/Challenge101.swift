ac5511e0aa8a5877c6081629a46c7ef81bc2aa5e
cham-s <cc.attou@gmail.com>
2019-03-03 04:19:39 +0100

    added c101

---
+++ challenges/Challenge101/Sources/Challenge101/Challenge101.swift
@@ -0,0 +1,3 @@
+struct Challenge101 {
+    var text = "Hello, World!"
+}
---
+++ challenges/Challenge101/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import Challenge101Tests
+
+var tests = [XCTestCaseEntry]()
+tests += Challenge101Tests.allTests()
+XCTMain(tests)
