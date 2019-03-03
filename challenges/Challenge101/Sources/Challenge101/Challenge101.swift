a71bd99226da2a1d97908fcb462dc42529775bbe
cham-s <cc.attou@gmail.com>
2019-03-03 04:32:00 +0100

    first version of c101

--- challenges/Challenge101/Sources/Challenge101/Challenge101.swift
+++ challenges/Challenge101/Sources/Challenge101/Challenge101.swift
@@ -1,3 +1,12 @@
-struct Challenge101 {
-    var text = "Hello, World!"
+import Foundation
+
+struct Pangram {
+    static func isPangram(_ input: String) -> Bool {
+        let lowered = input.lowercased()
+        let letters = "abcdefghijklmnopqrstuvwxyz"
+        let separator = CharacterSet(charactersIn: letters).inverted
+        let components = lowered.components(separatedBy: separator)
+            .filter { !$0.isEmpty }.reduce("", +)
+        return Set(components).count == 26
+    }
 }
