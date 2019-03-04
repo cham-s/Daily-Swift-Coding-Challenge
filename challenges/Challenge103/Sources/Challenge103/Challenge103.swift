099027038b33f686ffe5c116f2d7d739a8096536
cham-s <cc.attou@gmail.com>
2019-03-04 05:24:12 +0100

    c103 so far

--- challenges/Challenge103/Sources/Challenge103/Challenge103.swift
+++ challenges/Challenge103/Sources/Challenge103/Challenge103.swift
@@ -1,3 +1,25 @@
-struct Challenge103 {
-    var text = "Hello, World!"
+import Foundation
+
+
+class Node<T: Comparable> {
+    
 }
+
+class BinarySearchTree<T: Comparable> {
+    public var data: T
+    public var right: BinarySearchTree?
+    public var left: BinarySearchTree?
+    
+    init(_ value: T) {
+        self.data = value
+    }
+    
+    public func insert(_ value: T) {
+        var tmp: BinarySearchTree? =  self
+        while let current = tmp {
+            tmp = value < current.data ? current.left : current.right
+        }
+        tmp = BinarySearchTree(value)
+    }
+}
+
