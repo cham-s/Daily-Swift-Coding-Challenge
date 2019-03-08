77e0cef31b4ade75f6a4813e8facbd97b7796ea3
cham-s <cc.attou@gmail.com>
2019-03-08 07:27:14 +0100

    c105 so far

--- challenges/Challenge105/Sources/Challenge105/Challenge105.swift
+++ challenges/Challenge105/Sources/Challenge105/Challenge105.swift
@@ -1,3 +1,50 @@
-struct Challenge105 {
-    var text = "Hello, World!"
+import Foundation
+
+struct OCR {
+    enum OCRError: Error {
+        case invalidNumberOfColumns
+        case invalidNumberOfLines
+        case invaliCharacter
+    }
+    
+    private let correspondingOCR = [
+        "_ \n| |\n|_|\n   ": 0,
+        "   \n  |\n  |\n   ": 1,
+        " _ \n _|\n|_ \n   ": 2,
+        " _ \n _|\n _|\n   ": 3,
+        "   \n|_|\n  |\n   ": 4,
+        " _ \n|_ \n _|\n   ": 5,
+        " _ \n|_ \n|_|\n   ": 6,
+        
+        
+        
+        
+        ]
+    
+    private var input: String
+    
+    
+    
+    init(_ input: String) {
+        self.input = input
+    }
+    
+    public func converted() throws -> Int {
+        let invalidCharSet = CharacterSet(charactersIn: " _\n-").inverted
+        guard input.rangeOfCharacter(from: invalidCharSet) == nil else {
+            throw OCRError.invaliCharacter
+        }
+        
+        let splitted = input.split(separator: "\n")
+        guard splitted.count == 4 else {
+            throw OCRError.invalidNumberOfLines
+        }
+        
+        guard (splitted.first { $0.count != 3 }) == nil else {
+            throw OCRError.invalidNumberOfColumns
+        }
+        return 3
+    }
+    
+    
 }
