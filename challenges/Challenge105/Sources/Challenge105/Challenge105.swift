d38fc7e0e8dbb771d278045db227e6af5c47f08a
cham-s <cc.attou@gmail.com>
2019-03-10 20:57:03 +0100

    added few changes to col and lines

--- challenges/Challenge105/Sources/Challenge105/Challenge105.swift
+++ challenges/Challenge105/Sources/Challenge105/Challenge105.swift
@@ -1,8 +1,15 @@
+import Foundation
+
+public extension Int {
+    public func isDivisibleBy(_ number: Int) -> Bool {
+        return self % number == 0
+    }
+}
+
 struct OCR {
     enum OCRError: Error {
         case invalidNumberOfColumns
         case invalidNumberOfLines
-        case invaliCharacter
     }
     
     private let correspondingOCR = [
@@ -13,27 +20,31 @@ struct OCR {
         "   \n|_|\n  |\n   ": "4",
         " _ \n|_ \n _|\n   ": "5",
         " _ \n|_ \n|_|\n   ": "6",
-        ]
+        " _ \n  |\n  |\n   ": "7",
+        " _ \n|_|\n|_|\n   ": "8",
+        " _ \n|_|\n _|\n   ": "9"
+    ]
     
     private var input: String
-    private var columnCountIsValid: Bool {
-        return input.count % 3 == 0
-    }
+    private let maxColSize = 3
+    private let maxLineSize = 4
     
-    private func lines() throws -> [String] {
+    private var lines: [String] {
         return input.split(separator: "\n").map { String($0) }
     }
     
-    private func columns() throws -> [String] {
-        guard columnCountIsValid else { return [] }
-        let colCount = lines[0].count / 3
+    private var columns: [[String]] {
+        let isOK: (String) -> Bool = { $0.count.isDivisibleBy(self.maxColSize) }
+        guard let first = lines.first(where: isOK) else { return [] }
+        let colCount = first.count / maxColSize
         return (1..<colCount).reduce([]) { partial, colIndex in
             let m: [String] =  lines.map {
-                let start = $0.index($0.startIndex, offsetBy: colIndex * 3)
+                let start = $0.index($0.startIndex,
+                                     offsetBy: colIndex * maxColSize)
                 let endIndex = $0.index(start, offsetBy: 3)
                 return String($0[start..<endIndex])
             }
-            return partial + [m.joined(separator: "\n")]
+            return partial + [m]
         }
     }
     
@@ -42,18 +53,22 @@ struct OCR {
     }
     
     public func converted() throws -> String {
-        let splitted = input.split(separator: "\n")
-        guard splitted.count == 4 else {
-            throw OCRError.invalidNumberOfLines
+        var output = ""
+        
+        for col in columns {
+            let number = col.joined(separator: "\n")
+            guard number.count.isDivisibleBy(maxColSize)   else {
+                throw OCRError.invalidNumberOfColumns
+            }
+            guard col.count == maxLineSize else {
+                throw OCRError.invalidNumberOfLines
+            }
+            if let value = correspondingOCR[number] {
+                output += value
+            } else {
+                return "?"
+            }
         }
-        
-        guard (splitted.first { $0.count != 3 }) == nil else {
-            throw OCRError.invalidNumberOfColumns
-        }
-        
-        
-        return correspondingOCR[input] ?? "?"
+        return output
     }
-    
-    
 }
