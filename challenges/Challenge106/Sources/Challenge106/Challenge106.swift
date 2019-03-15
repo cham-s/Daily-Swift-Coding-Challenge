f45bb24b33f48fe842e9a6167a391c9718b797a4
cham-s <cc.attou@gmail.com>
2019-03-15 22:20:02 +0100

    c106 so far [ci skip]

--- challenges/Challenge106/Sources/Challenge106/Challenge106.swift
+++ challenges/Challenge106/Sources/Challenge106/Challenge106.swift
@@ -1,14 +1,30 @@
 func RomanNumeral(number: Int) -> String {
+    let roman = [
+        1 : "I",
+        2 : "II",
+        3 : "III",
+        4 : "IV",
+        5 : "V",
+        6 : "VI",
+        7 : "VII",
+        8 : "VIII",
+        9 : "IX",
+        10: "X",
+        50: "L",
+        100: "C",
+        1000: "M"
+    ]
     let strNumber = String(number).reversed()
-    let decomposed: [String] = []
-    let power = 0
+    var decomposed: [(Int, Int)] = []
+    var power = 0
     
-    for n in strNumber {
-        switch power {
-        case <#pattern#>:
-            <#code#>
-        default:
-            <#code#>
-        }
+    for value in strNumber {
+        let key = Int(pow(10.0, Double(power)))
+        decomposed.append((key, Int(String(value))!))
+        power += 1
     }
+    print(Array(decomposed.reversed()))
+    
+    
+    return ""
 }
