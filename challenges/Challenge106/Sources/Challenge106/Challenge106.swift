7d18874dfecfbb9e89966eb530f1364040177cbe
cham-s <cc.attou@gmail.com>
2019-03-15 21:16:02 +0100

    c106 so far

--- challenges/Challenge106/Sources/Challenge106/Challenge106.swift
+++ challenges/Challenge106/Sources/Challenge106/Challenge106.swift
@@ -1,3 +1,14 @@
-struct Challenge106 {
-    var text = "Hello, World!"
+func RomanNumeral(number: Int) -> String {
+    let strNumber = String(number).reversed()
+    let decomposed: [String] = []
+    let power = 0
+    
+    for n in strNumber {
+        switch power {
+        case <#pattern#>:
+            <#code#>
+        default:
+            <#code#>
+        }
+    }
 }
