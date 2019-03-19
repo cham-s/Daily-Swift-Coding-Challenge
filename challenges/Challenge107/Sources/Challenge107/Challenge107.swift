4c83de7e8ecc1a53a1eb6d4b700b1711392b3de4
cham-s <cc.attou@gmail.com>
2019-03-19 14:46:44 +0100

    first step done

--- challenges/Challenge107/Sources/Challenge107/Challenge107.swift
+++ challenges/Challenge107/Sources/Challenge107/Challenge107.swift
@@ -1,3 +1,27 @@
-struct Challenge107 {
-    var text = "Hello, World!"
+public extension Character {
+    public var alphabet : [Character] {
+        return Array("abcdefghijklmnopqrstuwxyz")
+    }
+    public var isAlpha: Bool {
+        return alphabet.contains(self) ||
+            String(alphabet).uppercased().contains(self)
+    }
+    public var isLowercase: Bool {
+        guard self.isAlpha else { return false }
+        return alphabet.contains(self)
+    }
+    public var isUppercase: Bool {
+        return isLowercase
+    }
+    
+    public var ceasared: Character? {
+        guard isAlpha else { return nil }
+        let ceasrOffSet = 3
+        let index = (alphabet.firstIndex(of: self)! + ceasrOffSet) % 26
+        return alphabet[index]
+    }
+    
+    public func ceasar(shift: Int) -> Character? {
+        
+    }
 }
