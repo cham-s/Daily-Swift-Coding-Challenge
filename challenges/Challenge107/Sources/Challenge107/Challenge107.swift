961c269562c8914f4e8cfb702ad9e34f1b280ee2
cham-s <cc.attou@gmail.com>
2019-03-20 15:04:35 +0100

    c107 so far

--- challenges/Challenge107/Sources/Challenge107/Challenge107.swift
+++ challenges/Challenge107/Sources/Challenge107/Challenge107.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 public extension Character {
     private var alphabet : [Character] {
         return Array("abcdefghijklmnopqrstuvwxyz")
@@ -16,21 +14,26 @@ public extension Character {
         return !isLowercase
     }
     
-    public func ceasar(shiftKey: Character = "d") -> Character? {
+    public func ceasar(shiftKey: Character = "d",
+                       decode: Bool = false) -> Character? {
         guard isAlpha else { return nil }
         let ceasrOffSet = alphabet.firstIndex(of: shiftKey)!
-        let index = (alphabet.firstIndex(of: self)! + ceasrOffSet) % 26
+        var index = 0
+        if decode {
+            index = abs((alphabet.firstIndex(of: self)! - ceasrOffSet))
+        } else {
+            index = (alphabet.firstIndex(of: self)! + ceasrOffSet) % 26
+        }
         return alphabet[index]
     }
 }
 
-let char = "iamapandabear".map { $0.ceasar()! }
-
-
 struct Cipher {
-    public var key: String
+    public var key: String?
     
-    init?(key: String = "d") {
+    
+    init?(key: String? = nil) {
+        guard let key = key else { return }
         guard !key.isEmpty else { return nil }
         guard key.first(where: { $0.isUppercase }) == nil else { return nil }
         guard key.first(where: { !$0.isAlpha }) == nil else { return nil }
@@ -38,14 +41,16 @@ struct Cipher {
     }
     
     public func encode(_ input: String) -> String? {
-        guard key.first(where: { !$0.isAlpha }) == nil else { return nil }
+        guard let key = key else { return String(input.map { $0.ceasar()! }) }
         guard input.count == key.count else { return nil }
-        
         return String(zip(input, key).map { $0.0.ceasar(shiftKey: $0.1)! })
     }
     
-    public func decode() {
-        
+    public func decode(_ input: String) -> String? {
+        guard let key = key else { return String(input.map { $0.ceasar(decode: true)! }) }
+        guard input.count == key.count else { return nil }
+        return String(zip(input, key).map {
+            $0.0.ceasar(shiftKey: $0.1, decode: true)!
+        })
     }
 }
-
