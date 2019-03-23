d5d2673d16a260ab2ee46e1e2e038621d8a3e647
cham-s <cc.attou@gmail.com>
2019-03-23 20:05:25 +0100

    added c108

---
+++ challenges/Challenge108/Sources/Challenge108/Challenge108.swift
@@ -0,0 +1,3 @@
+struct Challenge108 {
+    var text = "Hello, World!"
+}
---
+++ challenges/Challenge108/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import Challenge108Tests
+
+var tests = [XCTestCaseEntry]()
+tests += Challenge108Tests.allTests()
+XCTMain(tests)
