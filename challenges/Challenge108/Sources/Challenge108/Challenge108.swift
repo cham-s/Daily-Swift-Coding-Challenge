d2f0f7350962975ef68f3d4e59ce502b9d63c29e
cham-s <cc.attou@gmail.com>
2019-03-24 21:28:44 +0100

    c108 so far.

--- challenges/Challenge108/Sources/Challenge108/Challenge108.swift
+++ challenges/Challenge108/Sources/Challenge108/Challenge108.swift
@@ -1,3 +1,15 @@
-struct Challenge108 {
-    var text = "Hello, World!"
+struct Say {
+    public let number: String
+    
+    init(_ number: Int) {
+        self.number = String(number)
+    }
+    
+    public func say() -> String {
+        var decompose = number.reversed().enumerated().map {
+            ($0.offset, Int(String($0.element))!)
+            }.reversed()
+        print(decompose)
+        return ""
+    }
 }
